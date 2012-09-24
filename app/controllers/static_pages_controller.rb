781fa0eeb6cbf04559b6c411186a1012400afac8
yingCMU <ying.li@sv.cmu.edu>
2012-09-24 10:15:38 -0400

    9-24

--- app/controllers/static_pages_controller.rb
+++
@@ -1,8 +0,0 @@
-class StaticPagesController < ApplicationController
-  def home
-   # @posts = current_user.posts.paginate(page: params[:page])     if !current_user.nil?
-  end
-
-  def help
-  end
-end
